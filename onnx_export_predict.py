b9ce981e44f5d7ee8994bf775867b2cec29b6699
Hondlus <48700890+Hondlus@users.noreply.github.com>
2025-06-08 12:48:28 +0800

    Update onnx_export_predict.py

--- onnx_export_predict.py
+++ onnx_export_predict.py
@@ -10,7 +10,7 @@ all_detections = []
 # 加载模型
 model = YOLO('weights/yolo11n-seg.onnx')  # 使用ONNX模型
 
-input_dir = '/Users/hongliang/Desktop/yolov12/datasets/crack-seg/test/images'
+input_dir = '/Users/hongliang/Desktop/yolov12/datasets/crack-seg/test/images/1616.rf.c868709931a671796794fdbb95352c5a.jpg'
 output_dir = './output'
 
 os.makedirs(output_dir, exist_ok=True)
@@ -20,10 +20,10 @@ results = model.predict(
     source=input_dir,
     task='segment',
     imgsz=640,
-    # save=True,
-    # project=output_dir,
-    # exist_ok=True,
-    # name='exp',
+    save=True,
+    project=output_dir,
+    exist_ok=True,
+    name='exp',
 )
 
 for result in results:
@@ -34,4 +34,5 @@ for result in results:
 
     if result.masks is not None:
         masks_list = result.masks.xy
+        # masks_list = [mask.tolist() for mask in result.masks.xy]
         print('mask: ', masks_list)
