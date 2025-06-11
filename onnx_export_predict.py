7ecdea566414474172e7e2ea6532d8c7913b67ba
Hondlus <48700890+Hondlus@users.noreply.github.com>
2025-06-11 10:25:45 +0800

    Update onnx_export_predict.py

--- onnx_export_predict.py
+++ onnx_export_predict.py
@@ -2,16 +2,15 @@ import os
 from ultralytics import YOLO
 
 
-# model = YOLO("./weights/yolo11n-seg.pt")
+# model = YOLO("./weights/best.pt")
+#
+# model.export(format="onnx")
 
-# model.export(format="onnx")onnx
-
-all_detections = []
 # 加载模型
-model = YOLO('weights/yolo11n-seg.onnx')  # 使用ONNX模型
+model = YOLO('weights/best.onnx', task='segment')  # 使用ONNX模型
 
-input_dir = '/Users/hongliang/Desktop/yolov12/datasets/crack-seg/test/images/1616.rf.c868709931a671796794fdbb95352c5a.jpg'
-output_dir = './output'
+input_dir = 'C:/Users/dxw-user/Desktop/yolov12/input/1616.rf.c868709931a671796794fdbb95352c5a.jpg'
+output_dir = 'C:/Users/dxw-user/Desktop/yolov12/output'
 
 os.makedirs(output_dir, exist_ok=True)
 
@@ -29,10 +28,10 @@ results = model.predict(
 for result in results:
     if result.boxes is not None:
         boxes_array = result.boxes.xyxy.cpu().numpy()
-        boxes_list = boxes_array.tolist()
+        boxes_list = boxes_array.astype(int).tolist()
         print('box: ', boxes_list)
 
     if result.masks is not None:
-        masks_list = result.masks.xy
-        # masks_list = [mask.tolist() for mask in result.masks.xy]
+        # masks_list = result.masks.xy
+        masks_list = [mask.astype(int).tolist() for mask in result.masks.xy]
         print('mask: ', masks_list)
